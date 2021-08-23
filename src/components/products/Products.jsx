2c80861f689dfefedee12ce2f688e94b4d4fff23
Nariman <onlnaovnar@gmail.com>
2021-08-23 12:48:47 +0600

    added cart section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './product/Product'
+import useStyles from './styles'
 
-const products = [
-  {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.701xh;0,0.229xh&resize=1200:*'},
-  {id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10', url: 'https://www.ixbt.com/img/r30/00/02/25/30/Apple16inchMacBookPro111319large.jpg'},
-]
 
-const Products = () => {
+const Products = ({ products, onAddToCard }) => {
+  const classes = useStyles();
+
   return (
-    <main>
-      <Grid container justifyContent="center" spacing={4}>
-        {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product}/>
-          </Grid>
-        ))}
-      </Grid>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
+        <Grid container justifyContent="center" spacing={4}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Product product={product} onAddToCard={onAddToCard}/>
+            </Grid>
+          ))}
+        </Grid>
   </main>
   );
 
